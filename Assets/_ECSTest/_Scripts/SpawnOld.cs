cf42d92da02ce6dd4fe0e6fc275170d74a7ef0c6
Sourav Chatterjee <sromic@gmail.com>
2020-08-04 19:09:40 +0530

    Added the DOTS packages into the scene. Added some elements to scene to test FPS parallel and Sequential processing.

---
+++ Assets/_ECSTest/_Scripts/SpawnOld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOld : MonoBehaviour
+{
+    [Range(0, 15000)] [SerializeField] private int maxSheep;
+    [SerializeField] private GameObject sheepPrefab;
+    private Transform[] sheepTransforms;
+
+    void Start()
+    {
+        sheepTransforms = new Transform[maxSheep];
+        for (int i = 0; i < maxSheep; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            GameObject sheep = Instantiate(sheepPrefab, pos, Quaternion.identity);
+            sheepTransforms[i] = sheep.transform;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < sheepTransforms.Length; i++)
+        {
+            sheepTransforms[i].Translate(0, 0, Time.deltaTime);
+            if (sheepTransforms[i].position.z > 50)
+            {
+                sheepTransforms[i].transform.position = new Vector3(sheepTransforms[i].transform.position.x, 0, -50);
+            }
+        }   
+    }
+}
---
+++ Assets/_ECSTest/_Scripts/SpawnOldJobs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
+
+public class SpawnOldJobs : MonoBehaviour
+{
+    [Range(0, 15000)] [SerializeField] private int _maxSheep;
+    [SerializeField] private GameObject _sheepPrefab;
+    [SerializeField] private Transform _sheepHolder;
+    private Transform[] _sheepTransforms;
+
+    struct MoveJob : IJobParallelForTransform
+    {
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += 0.1f * (transform.rotation * new Vector3(0, 0 , 1));
+            if (transform.position.z > 50)
+            {
+                transform.position = new Vector3(transform.position.x, 0, -50);
+            }
+        }
+    }
+
+    private MoveJob _moveJob;
+    private JobHandle _moveHandle;
+    private TransformAccessArray _transforms;
+    
+    private void Start()
+    {
+        _sheepTransforms = new Transform[_maxSheep];
+        for (int i = 0; i < _maxSheep; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            GameObject sheep = Instantiate(_sheepPrefab, pos, Quaternion.identity);
+            sheep.transform.SetParent(_sheepHolder);
+            _sheepTransforms[i] = sheep.transform;
+        }
+        _transforms = new TransformAccessArray(_sheepTransforms);
+    }
+
+    private void Update()
+    {
+        _moveJob = new MoveJob { };
+        _moveHandle = _moveJob.Schedule(_transforms);
+    }
+
+    private void LateUpdate()
+    {
+        _moveHandle.Complete();
+    }
+
+    private void OnDestroy()
+    {
+        _transforms.Dispose();
+    }
+}
